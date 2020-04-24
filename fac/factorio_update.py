31f86c7544e6205d054fa5e2a2db0de2de70e2b1
LucaPierino <smartino84@gmail.com>
2020-04-24 18:29:10 +0200

    seccond attempt: now it works

--- fac/factorio_update.py
+++ fac/factorio_update.py
@@ -2,59 +2,47 @@ import os
 import shutil
 import sys
 import urllib3
-import tarfile
 
-
-factorio_stable = "https://factorio.com/get-download/stable/headless/linux64"
-
+factorio_stable = "https://factorio.com/get-download/stable/headless/linux64" # url to download latst-stable for future implementation
 factorio_path = input('Specify Factorio path ex. \n "/home/user/factorio/" --> ')  # get factorio path
 try:
     os.chdir(factorio_path)
 except Exception as e:
     print("Path not found: " + factorio_path)
     print(e)
-
 factorio_version = input('Factorio version \n ex. "0.18.xx" --> ')  # get factorio version
+path_file = factorio_path+factorio_version
 print("Factorio path : " + factorio_path)
 print("Factorio version : " + factorio_version)
+print("Temp folder : " + path_file)
+
 factorio_latestexperimental = "https://factorio.com/get-download/" + factorio_version + "/headless/linux64"
-
-
 http = urllib3.PoolManager()
-r = http.request('GET', factorio_latestexperimental, preload_content=False)
-path_file = factorio_path+factorio_version
+resp = http.request('GET', factorio_latestexperimental, preload_content=False)
+with open(factorio_path + "/linux64", 'wb') as out:
+    while True:
+        data = resp.read(2048)
+        if not data:
+            break
+        out.write(data)
+resp.release_conn()
 #temp dir to move all extracted data /home/user/factorio/0.xx.xx/
 try:
     os.mkdir(path_file)
-    shutil.unpack_archive(factorio_path+"linux64.tar", path_file, "tar")
+    shutil.unpack_archive(factorio_path + "linux64", path_file, "tar")
 except OSError:
     print("Creation of the directory %s failed" % path_file)
 else:
     print("Successfully created the directory %s " % path_file)
-r.release_conn()
-path = path_file+"/factorio/"
-dest = shutil.move(path, factorio_path)
-print("Destination path:", dest)
+path = ["/factorio/bin/","/factorio/data/","/factorio/config-path.cfg"]
+for x in path:
+    abs_path = path_file + x
+    dest = shutil.move(abs_path, factorio_path)
+    print("Destination path:", dest)
+try:
+    os.remove(factorio_path+"/linux64")
+    shutil.rmtree(path_file)
+except OSError as e:
+    print("Error: %s : %s" % (path_file, e.strerror))
 
 
-"""
-
-cd $factorio_path
-
-echo Inserire per intero la versione che si desidera installare:
-read version_number
-#echo $version_number
-
-
-
-#estrazione e rimozione file archivio
-mkdir $factorio_path/factorio-$version_number/ 
-tar -xf linux64 --directory $factorio_path/factorio-$version_number/
-rm linux64
-#copio i file appena scaricati
-cp -R factorio-$version_number/factorio/* .
-
-#cancello la dir temporanea del download
-rm -rf $factorio_path/factorio-$version_number
-
-"""
