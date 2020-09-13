a5808e6ffdde3053d048f5ff23f6a536170e5de1
Clarence "Sparr" Risher <sparr0@gmail.com>
2020-09-13 16:49:21 -0700

    Fix recognition of 0.18 1.0 compatibility

--- fac/commands/list.py
+++ fac/commands/list.py
@@ -70,7 +70,8 @@ class ListCommand(Command):
                 tags.append('unpacked')
             if mod.held:
                 tags.append('held')
-            if mod.game_version != self.config.game_version_major:
+            if (mod.game_version != self.config.game_version_major) and \
+            (mod.game_version != '0.18' or self.config.game_version_major != '1.0'):
                 tags.append('incompatible')
 
             if any(tag in tags for l in args.exclude for tag in l) or \
